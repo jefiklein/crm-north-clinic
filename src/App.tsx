82f6d5fb1c9f160d0dafad679d19e8b6f7d4284e
[dyad] <jefiklein@gmail.com>
2025-05-19 23:32:58 -0300

    [dyad] wrote 1 file(s), deleted 3 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -67,8 +66,8 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
-        <Toaster />
-        <Sonner />
+        {/* Removed shadcn/ui Toaster */}
+        <Sonner /> {/* Keep Sonner from react-hot-toast */}
         <BrowserRouter>
           <Routes>
             {/* Login Page - Renders if not logged in */}
